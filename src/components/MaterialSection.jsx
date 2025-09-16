379709990d8aaa983945a8aa2a9128abdabbf941
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-09-16 17:38:19 +0100

    update

---
+++ src/components/MaterialSection.jsx
@@ -0,0 +1,12 @@
+function MaterialSection() {
+  return (
+    <div className="material-section">
+      <div className="material-content"></div>
+      <div className="material-images">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+export default MaterialSection;
