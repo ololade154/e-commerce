036f4771587d851b05bb86f619712997e1c35109
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-09-18 14:17:37 +0100

    update

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -4,9 +4,42 @@ function Testimonial() {
       <h1>testimonials</h1>
       <h2>Our Client Reviews</h2>
       <div className="testimonial-images">
-        <div className="testimonial-image"></div>
-        <div className="testimonail-image"></div>
-        <div className="testimonial-image"></div>
+        <div className="testimonial-image">
+          <img src="/assets/testimonial-image1.svg" />
+          <div className="testimonial-content">
+            <img src="/assets/avatar1.svg" className="testimonial-avatar" />
+
+            <p>
+              “Terimakasih banyak, kini ruanganku menjadi lebih mewah dan
+              terlihat mahal”
+            </p>
+            <div className="stars">⭐⭐⭐⭐⭐</div>
+          </div>
+        </div>
+        <div className="testimonial-image">
+          <img src="/assets/testimonial-image2.svg" />
+          <div className="testimonial-content">
+            <img src="/assets/avatar1.svg" className="testimonial-avatar" />
+
+            <p>
+              “Terimakasih banyak, kini ruanganku menjadi lebih mewah dan
+              terlihat mahal”
+            </p>
+            <div className="stars">⭐⭐⭐⭐⭐</div>
+          </div>
+        </div>
+        <div className="testimonial-image">
+          <img src="/assets/testimonial-image3.svg" />
+          <div className="testimonial-content">
+            <img src="/assets/avatar1.svg" className="testimonial-avatar" />
+
+            <p>
+              “Terimakasih banyak, kini ruanganku menjadi lebih mewah dan
+              terlihat mahal”
+            </p>
+            <div className="stars">⭐⭐⭐⭐⭐</div>
+          </div>
+        </div>
       </div>
     </div>
   );
