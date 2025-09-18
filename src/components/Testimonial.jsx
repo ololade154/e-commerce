3963a72993dc80f77dee8c2eb0e29804492f650d
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-09-18 16:44:20 +0100

    update

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -5,39 +5,46 @@ function Testimonial() {
       <h2>Our Client Reviews</h2>
       <div className="testimonial-images">
         <div className="testimonial-image">
-          <img src="/assets/testimonial-image1.svg" />
+          <img src="/assets/testimonial-image1.svg" className="avatar-image" />
           <div className="testimonial-content">
             <img src="/assets/avatar1.svg" className="testimonial-avatar" />
-
-            <p>
-              “Terimakasih banyak, kini ruanganku menjadi lebih mewah dan
-              terlihat mahal”
-            </p>
-            <div className="stars">⭐⭐⭐⭐⭐</div>
+            <div className="avatar-content">
+              <h3>Bang Upin</h3>
+              <span>Pedagang Asongan</span>
+              <p>“Terimakasih banyak, kini ruanganku</p>
+              <p>menjadi lebih mewah dan terlihat</p>
+              <p>mehal"</p>
+              <div className="stars">⭐⭐⭐⭐⭐</div>
+            </div>
           </div>
         </div>
         <div className="testimonial-image">
-          <img src="/assets/testimonial-image2.svg" />
+          <img src="/assets/testimonial-image2.svg" className="avatar-image" />
           <div className="testimonial-content">
-            <img src="/assets/avatar1.svg" className="testimonial-avatar" />
-
-            <p>
-              “Terimakasih banyak, kini ruanganku menjadi lebih mewah dan
-              terlihat mahal”
-            </p>
-            <div className="stars">⭐⭐⭐⭐⭐</div>
+            <img src="/assets/avatar2.svg" className="testimonial-avatar" />
+            <div className="avatar-content">
+              <h3>Ibuk Sukijan</h3>
+              <span>Ibu Rumah Tangga</span>
+              <p>"Makasih Panto, aku sekarang berasa</p>
+              <p>tinggnal di apartment karena barang-</p>
+              <p>barang yang terlihat mewah"</p>
+              <div className="stars">⭐⭐⭐⭐⭐</div>
+            </div>
           </div>
         </div>
         <div className="testimonial-image">
-          <img src="/assets/testimonial-image3.svg" />
+          <img src="/assets/testimonial-image3.svg" className="avatar-image" />
           <div className="testimonial-content">
-            <img src="/assets/avatar1.svg" className="testimonial-avatar" />
+            <img src="/assets/avatar3.svg" className="testimonial-avatar" />
+            <div className="avatar-content">
+              <h3>Mpok Ina </h3>
+              <span>Karyawan Swasta</span>
+              <p>"Sangat terjangkau untuk kantong</p>
+              <p>saya yang tidak terlalu banyak</p>
+              <p>saya yang tidak terlalu banyak"</p>
 
-            <p>
-              “Terimakasih banyak, kini ruanganku menjadi lebih mewah dan
-              terlihat mahal”
-            </p>
-            <div className="stars">⭐⭐⭐⭐⭐</div>
+              <div className="stars">⭐⭐⭐⭐⭐</div>
+            </div>
           </div>
         </div>
       </div>
